5d7fc46ec29cda3a19ba91d3ee575972934764f0
Jonathan Roy <jroy@optimumweb.ca>
2020-11-30 21:41:55 -0500

    dev & design

--- inc/wpbp-custom.php
+++ inc/wpbp-custom.php
@@ -18,3 +18,26 @@ wpbp_register_sidebars( array( 'Front Page Hero' ) );
 register_nav_menus( array(
     'header_cta_navigation' => __("Header CTA Navigation", 'optimumweb-wp'),
 ) );
+
+// AOS fix
+add_action('wpbp_head', 'insert_aos_fix');
+function insert_aos_fix()
+{ ?>
+    <noscript>
+        <style type="text/css">
+            [data-aos] {
+                opacity: 1 !important;
+                transform: translate(0) scale(1) !important;
+            }
+        </style>
+    </noscript>
+
+    <style type="text/css">
+        @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+            [data-aos] {
+                opacity: 1 !important;
+                transform: translate(0) scale(1) !important;
+            }
+        }
+    </style>
+<?php }
