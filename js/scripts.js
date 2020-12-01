4dfd2c0a8ab8c2e72570af5f1bd16301fd10246f
Jonathan Roy <jroy@optimumweb.ca>
2020-11-30 21:38:50 -0500

    dev & design

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ $(document).ready(function() {
         $html = $('html'),
         $body = $('body');
 
+    if (typeof AOS === 'object') {
+        AOS.init({
+            once: true,
+            duration: 800,
+            delay: 100
+        });
+    }
+
     $window.on('scroll resize check-scroll', function () {
 
         $window.scrollTop() > 0 ? $html.addClass('is-scrolled') : $html.removeClass('is-scrolled');
