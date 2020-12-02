b0299029faaddf2ab83ab1794544b3bcf402819e
Jonathan Roy <jroy@optimumweb.ca>
2020-12-02 15:05:20 -0500

    dev & design

--- loop.php
+++ loop.php
@@ -11,40 +11,42 @@
 <?php /* Start loop */ ?>
 <?php while ( have_posts() ) : the_post(); ?>
     <?php wpbp_post_before(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class('loop'); ?>>
-        <?php wpbp_post_inside_before(); ?>
-        <?php if ( has_post_thumbnail() ) : ?>
-            <div class="post-thumbnail">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_post_thumbnail( 'thumbnail' ); ?>
-                </a>
-            </div>
-        <?php endif; ?>
-        <header class="post-header">
-            <h2 class="post-title">
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </h2>
-            <div class="post-meta">
-                <time class="post-date updated" datetime="<?php the_time( 'c' ); ?>">
-                    <?php printf( __( 'Posted on %s', 'wpbp' ), get_the_time( __( 'l, F jS, Y', 'wpbp' ) ) ); ?>
-                </time>
-                <span class="post-author byline author vcard">
-                    <?php _e( 'by', 'wpbp' ); ?> <?php the_author_posts_link(); ?>
-                </span>
-            </div>
-        </header>
-        <div class="post-content">
-            <?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search ?>
-                <?php the_excerpt(); ?>
-            <?php else : ?>
-                <?php the_content(); ?>
+    <div class="post-container">
+        <article id="post-<?php the_ID(); ?>" <?php post_class('loop'); ?>>
+            <?php wpbp_post_inside_before(); ?>
+            <?php if ( has_post_thumbnail() ) : ?>
+                <div class="post-thumbnail">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail( 'thumbnail' ); ?>
+                    </a>
+                </div>
             <?php endif; ?>
-        </div>
-        <footer class="post-footer">
-            <p class="post-tags"><?php the_tags(); ?></p>
-        </footer>
-        <?php wpbp_post_inside_after(); ?>
-    </article>
+            <header class="post-header">
+                <h2 class="post-title">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+                <div class="post-meta">
+                    <time class="post-date updated" datetime="<?php the_time( 'c' ); ?>">
+                        <?php printf( __( 'Posted on %s', 'wpbp' ), get_the_time( __( 'l, F jS, Y', 'wpbp' ) ) ); ?>
+                    </time>
+                    <span class="post-author byline author vcard">
+                        <?php _e( 'by', 'wpbp' ); ?> <?php the_author_posts_link(); ?>
+                    </span>
+                </div>
+            </header>
+            <div class="post-content">
+                <?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search ?>
+                    <?php the_excerpt(); ?>
+                <?php else : ?>
+                    <?php the_content(); ?>
+                <?php endif; ?>
+            </div>
+            <footer class="post-footer">
+                <p class="post-tags"><?php the_tags(); ?></p>
+            </footer>
+            <?php wpbp_post_inside_after(); ?>
+        </article>
+    </div>
     <?php wpbp_post_after(); ?>
 <?php endwhile; // End the loop ?>
 
