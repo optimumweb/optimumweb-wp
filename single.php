2d868fbb993da760bc412528d156b63533b38547
Jonathan Roy <jroy@optimumweb.ca>
2020-12-02 12:32:52 -0500

    dev & design

--- single.php
+++ single.php
@@ -1,39 +1,59 @@
 <?php get_header(); ?>
-<?php wpbp_content_before(); ?>
-<section class="hero">
-    <div class="hero-cover" style="background-image: url('<?php echo get_featured_image_url(); ?>');">
-        <div class="hero-overlay is-grey-dark">
-            <div class="hero-padding">
-                <div class="<?php wpbp_container_class(); ?>">
-                    <div class="grid_12">
-                        <h1 class="page-title">
-                            <?php the_title(); ?>
-                        </h1>
-                        <?php if ( $metadesc = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true) ) : ?>
-                            <p class="lead">
-                                <?php echo $metadesc; ?>
-                            </p>
-                        <?php endif; ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+    <?php wpbp_post_before(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php wpbp_post_inside_before(); ?>
+        <section class="hero">
+            <div class="hero-cover" style="background-image: url('<?php echo get_featured_image_url(); ?>');">
+                <div class="hero-overlay is-grey-dark">
+                    <div class="hero-padding">
+                        <div class="<?php wpbp_container_class(); ?>">
+                            <div class="grid_12">
+                                <h1 class="post-title">
+                                    <?php the_title(); ?>
+                                </h1>
+                                <div class="post-meta">
+                                    <time class="post-date updated" datetime="<?php the_time( 'c' ); ?>">
+                                        <?php printf( __( 'Posted on %s', 'wpbp' ), get_the_time( __( 'l, F jS, Y', 'wpbp' ) ) ); ?>
+                                    </time>
+                                    <span class="post-author byline author vcard">
+                                        <?php _e( 'by', 'wpbp' ); ?> <?php the_author_posts_link(); ?>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</section>
-<section id="content">
-    <?php wpbp_main_before(); ?>
-    <section id="main" role="main">
-        <?php wpbp_main_inside_before(); ?>
-        <div class="<?php wpbp_container_class(); ?>">
-            <div class="grid_12">
-                <?php wpbp_loop_before(); ?>
-                <?php get_template_part( 'loop', 'single' ); ?>
-                <?php wpbp_loop_after(); ?>
-            </div>
-        </div>
-        <?php wpbp_main_inside_after(); ?>
-    </section>
-    <?php wpbp_main_after(); ?>
-</section>
-<?php wpbp_content_after(); ?>
+        </section>
+        <?php wpbp_content_before(); ?>
+        <section id="content">
+            <?php wpbp_main_before(); ?>
+            <section id="main" role="main">
+                <?php wpbp_main_inside_before(); ?>
+                <div class="<?php wpbp_container_class(); ?>">
+                    <div class="grid_12">
+                        <?php wpbp_loop_before(); ?>
+                        <div class="post-content">
+                            <?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search ?>
+                                <?php the_excerpt(); ?>
+                            <?php else : ?>
+                                <?php the_content(); ?>
+                            <?php endif; ?>
+                        </div>
+                        <footer class="post-footer">
+                            <p class="post-tags"><?php the_tags(); ?></p>
+                        </footer>
+                        <?php wpbp_loop_after(); ?>
+                    </div>
+                </div>
+                <?php wpbp_main_inside_after(); ?>
+            </section>
+            <?php wpbp_main_after(); ?>
+        </section>
+        <?php wpbp_content_after(); ?>
+    </article>
+    <?php wpbp_post_after(); ?>
+<?php endwhile; ?>
 <?php get_footer(); ?>
